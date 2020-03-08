fc498559109d9ee85e52f75b36dc85c69a956084
Gabriel <gvogt@asu.edu>
2020-03-08 16:37:57 -0700

    completed employee and program methods. added comments and regions

--- VogtPayroll/Employee.cs
+++ VogtPayroll/Employee.cs
@@ -6,10 +6,12 @@ namespace VogtPayroll
 {
     class Employee
     {
+        // Fields
         private string _name;
         private int _totalWorkingHours;
         private decimal _payrate;
 
+        // CTOR
         public Employee(string name, int totalWorkingHours, decimal payRate)
         {
             _name = name;
@@ -17,49 +19,93 @@ namespace VogtPayroll
             _payrate = payRate;
         }
 
-        public Employee GetEmpInfo()
-        {
-            // allow user to enter employee name, total working hours and payrate
-
-            Console.WriteLine("What is the employee's name?");
-            var empName = Console.ReadLine();
-
-            Console.WriteLine("What is the employee's total working hours?");
-            var empTotalWorkingHours = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("What is the employee's payrate?");
-            var empPayRate = Convert.ToDecimal(Console.ReadLine());
-
-            Employee employee = new Employee(empName, empTotalWorkingHours, empPayRate);
-
-            return employee;
-
-        }
-
-        public void ComputeGrossPay(Employee employee)
+        #region ComputeGrossPay
+        /// <summary>
+        /// Computes gross pay
+        /// </summary>
+        /// <returns>gross pay</returns>
+        private decimal ComputeGrossPay()
         {
             // compute gross pay use formula Employee gross pay = hours worked times hourly payrate
             decimal grossPay;
 
+            grossPay = _totalWorkingHours * _payrate;
+
+            return grossPay;
 
         }
+        #endregion
 
-        public void ComputePaidFedTax()
+        #region ComputePaidFedTax
+        /// <summary>
+        /// Computes paid federal tax
+        /// </summary>
+        /// <returns>paid fed tax</returns>
+        private decimal ComputePaidFedTax()
         {
+            decimal grossPay;
+            decimal paidFedTax;
             // compute amount of money emp should pay as fed tax. use formula employee paid fed taxes = 30% of gross pay
-        }
 
-        public void ComputePaidStateTax()
+            grossPay = ComputeGrossPay();
+
+            paidFedTax = grossPay * .30m;
+
+            return paidFedTax;
+
+        }
+        #endregion
+
+        #region ComputePaidStateTax
+        /// <summary>
+        /// Computes paid state tax
+        /// </summary>
+        /// <returns>paid state tax</returns>
+        private decimal ComputePaidStateTax()
         {
+            decimal grossPay;
+            decimal paidStateTax;
             // compute money which should pay as state tax. use formula emp paid state taxes 10% of gross pay
-        }
 
-        public void ComputeNetPay()
+            grossPay = ComputeGrossPay();
+
+            paidStateTax = grossPay * .10m;
+
+            return paidStateTax;
+
+        }
+        #endregion
+
+        #region ComputeNetPay
+        /// <summary>
+        /// Computes net pay
+        /// </summary>
+        /// <returns>net pay</returns>
+        private decimal ComputeNetPay()
         {
+            decimal grossPay;
+            decimal netPay;
+            decimal paidFedTax;
+            decimal paidStateTax;
             // compute net pay. use formula employee net pay = employee gross pay - (paid fed tax + paid state tax)
-        }
 
-        public void PrintEmpInfo()
+            grossPay = ComputeGrossPay();
+            paidFedTax = ComputePaidFedTax();
+            paidStateTax = ComputePaidStateTax();
+
+            netPay = grossPay - (paidFedTax + paidStateTax);
+
+            return netPay;
+
+        }
+        #endregion region
+
+        #region PrintEmpInfo
+        /// <summary>
+        /// Prints employee information
+        /// </summary>
+        /// <param name="employee">employee object passed in</param>
+        public void PrintEmpInfo(Employee employee)
         {
             // emp name
             // emp total working hours
@@ -67,7 +113,15 @@ namespace VogtPayroll
             // emp paid fed taxes
             // emp paid state taxes
             // emp net pay
+            Console.WriteLine($"Employee name: {employee._name}");
+            Console.WriteLine($"Employee total working hours: {employee._totalWorkingHours}");
+            Console.WriteLine($"Employee payrate: {employee._payrate}");
+            Console.WriteLine($"Federal Taxes Paid: {ComputePaidFedTax()}");
+            Console.WriteLine($"State Taxes Paid: {ComputePaidStateTax()}");
+            Console.WriteLine($"Netpay: {ComputeNetPay()}");
+
         }
+        #endregion
 
     }
 }
