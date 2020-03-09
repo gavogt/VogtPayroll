4eb8c2346fca01e0ae65da12db474329edea0621
Gabriel <gvogt@asu.edu>
2020-03-08 17:10:33 -0700

    removed unnecessary gross pay and added a math min to total working hours

--- VogtPayroll/Employee.cs
+++ VogtPayroll/Employee.cs
@@ -15,7 +15,7 @@ namespace VogtPayroll
         public Employee(string name, int totalWorkingHours, decimal payRate)
         {
             _name = name;
-            _totalWorkingHours = totalWorkingHours;
+            _totalWorkingHours = Math.Min(totalWorkingHours, 40);
             _payrate = payRate;
         }
 
@@ -27,11 +27,8 @@ namespace VogtPayroll
         private decimal ComputeGrossPay()
         {
             // compute gross pay use formula Employee gross pay = hours worked times hourly payrate
-            decimal grossPay;
 
-            grossPay = _totalWorkingHours * _payrate;
-
-            return grossPay;
+            return _totalWorkingHours * _payrate;
 
         }
         #endregion
@@ -43,15 +40,12 @@ namespace VogtPayroll
         /// <returns>paid fed tax</returns>
         private decimal ComputePaidFedTax()
         {
-            decimal grossPay;
-            decimal paidFedTax;
             // compute amount of money emp should pay as fed tax. use formula employee paid fed taxes = 30% of gross pay
-
+            decimal grossPay;
+            
             grossPay = ComputeGrossPay();
 
-            paidFedTax = grossPay * .30m;
-
-            return paidFedTax;
+            return grossPay * .30m;
 
         }
         #endregion
@@ -63,15 +57,12 @@ namespace VogtPayroll
         /// <returns>paid state tax</returns>
         private decimal ComputePaidStateTax()
         {
-            decimal grossPay;
-            decimal paidStateTax;
             // compute money which should pay as state tax. use formula emp paid state taxes 10% of gross pay
+            decimal grossPay;
 
             grossPay = ComputeGrossPay();
 
-            paidStateTax = grossPay * .10m;
-
-            return paidStateTax;
+            return grossPay * .10m;
 
         }
         #endregion
@@ -83,19 +74,16 @@ namespace VogtPayroll
         /// <returns>net pay</returns>
         private decimal ComputeNetPay()
         {
+            // compute net pay. use formula employee net pay = employee gross pay - (paid fed tax + paid state tax)
             decimal grossPay;
-            decimal netPay;
             decimal paidFedTax;
             decimal paidStateTax;
-            // compute net pay. use formula employee net pay = employee gross pay - (paid fed tax + paid state tax)
-
+            
             grossPay = ComputeGrossPay();
             paidFedTax = ComputePaidFedTax();
             paidStateTax = ComputePaidStateTax();
 
-            netPay = grossPay - (paidFedTax + paidStateTax);
-
-            return netPay;
+            return grossPay - (paidFedTax + paidStateTax); 
 
         }
         #endregion region
@@ -107,12 +95,7 @@ namespace VogtPayroll
         /// <param name="employee">employee object passed in</param>
         public void PrintEmpInfo(Employee employee)
         {
-            // emp name
-            // emp total working hours
-            // emp hourly pay
-            // emp paid fed taxes
-            // emp paid state taxes
-            // emp net pay
+
             Console.WriteLine($"Employee name: {employee._name}");
             Console.WriteLine($"Employee total working hours: {employee._totalWorkingHours}");
             Console.WriteLine($"Employee payrate: {employee._payrate}");
